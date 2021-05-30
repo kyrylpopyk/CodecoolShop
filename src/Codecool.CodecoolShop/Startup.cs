da289f27ef79982e66249da1438e050b9945765a
bart capote <gauclee@gmail.com>
2021-05-30 16:30:21 +0200

    Add in memory data generator and use it in Startup.cs

--- src/Codecool.CodecoolShop/Startup.cs
+++ src/Codecool.CodecoolShop/Startup.cs
@@ -59,24 +59,18 @@ namespace Codecool.CodecoolShop
                     pattern: "{controller=Product}/{action=Index}/{id?}");
             });
 
-            SetupInMemoryDatabases();
-        }
+            // Generate In Memory Data: 
 
-        private void SetupInMemoryDatabases()
-        {
-            //IProductDao productDataStore = ProductDaoMemory.GetInstance();
-            //IProductCategoryDao productCategoryDataStore = ProductCategoryDaoMemory.GetInstance();
-            //ISupplierDao supplierDataStore = SupplierDaoMemory.GetInstance();
+            //1. Find the service layer within our scope.
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                //2. Get the instance of InMemoryDb in our services layer
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<InMemoryDb>();
 
-            //Supplier amazon = new Supplier{Name = "Amazon", Description = "Digital content and services"};
-            //supplierDataStore.Add(amazon);
-            //Supplier lenovo = new Supplier{Name = "Lenovo", Description = "Computers"};
-            //supplierDataStore.Add(lenovo);
-            //ProductCategory tablet = new ProductCategory {Name = "Tablet", Department = "Hardware", Description = "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display." };
-            //productCategoryDataStore.Add(tablet);
-            //productDataStore.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
-            //productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
-            //productDataStore.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
+                //3. Call the DataGenerator to create sample data
+                InMemoryDataGenerator.Init(context); // InMemory Data Generation
+            }
         }
     }
 }
