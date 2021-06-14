3abaa365259a4254f94185147d606b797572596e
bart capote <gauclee@gmail.com>
2021-06-14 17:46:52 +0200

    Set proper product names in db initialization query

--- src/EFDataAccessLibrary/Queries/StartDatabase.sql
+++ src/EFDataAccessLibrary/Queries/StartDatabase.sql
@@ -7,10 +7,10 @@ VALUES('Veco', 'New supplier');
 
 
 INSERT INTO Products(Currency, DefaultPrice, ProductCategoryId, SupplierId, Name, Description)
-VALUES('USD', 56, 1, 1, 'pRODUCT1', 'Fajny product 1');
+VALUES('USD', 56, 1, 1, 'Amazon Fire HD 8', 'Fajny product 1');
 
 INSERT INTO Products(Currency, DefaultPrice, ProductCategoryId, SupplierId, Name, Description)
-VALUES('USD', 92, 1, 1, 'pRODUCT2', 'Fajny product 2');
+VALUES('USD', 92, 1, 1, 'Amazon Fire', 'Fajny product 2');
 
 INSERT INTO Products(Currency, DefaultPrice, ProductCategoryId, SupplierId, Name, Description)
-VALUES('USD', 295, 1, 1, 'pRODUCT3', 'Fajny product 3');
+VALUES('USD', 295, 1, 1, 'Lenovo IdeaPad Miix 700', 'Fajny product 3');
